7f0444aabd2b9edad4393c41f2c8beabf5a38887
Kave Haj <kave.haj@snapp.cab>
2023-11-18 16:42:21 +0330

    added register user

--- main.go
+++ main.go
@@ -32,6 +32,34 @@ func main() {
 		scanner.Scan()
 		category = scanner.Text()
 		fmt.Println("task :", name, category, duedate)
-	}
+	} else if *command == "create-category" {
+		var title, color string
 
+		fmt.Println("please enter the task title: ")
+		scanner.Scan()
+		title = scanner.Text()
+
+		fmt.Println("please enter the task color: ")
+		scanner.Scan()
+		color = scanner.Text()
+
+		fmt.Println("category", title, color)
+	} else if *command == "register-user" {
+		var id, email, password string
+
+		fmt.Println("please enter the task id: ")
+		scanner.Scan()
+		id = scanner.Text()
+
+		fmt.Println("please enter the task email: ")
+		scanner.Scan()
+		email = scanner.Text()
+
+		fmt.Println("please enter the task password: ")
+		scanner.Scan()
+		password = scanner.Text()
+
+		fmt.Println("register :", id, email, password)
+
+	}
 }
