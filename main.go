de5d874589c3256ff6e00b9c188664a21a69082b
Kave Haj <kave.haj@snapp.cab>
2023-11-18 15:26:09 +0330

    start project :D

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	fmt.Println("hello to TODO app")
+
+	command := flag.String("create-todo-task", "no", "createing the todo task")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println()
+
+	if *command == "create-todo-task" {
+		var name, duedate, category string
+
+		fmt.Println("Please Enter the task title")
+		scanner.Scan()
+		name = scanner.Text()
+
+		fmt.Println("Please Enter the taske duration")
+		scanner.Scan()
+		duedate = scanner.Text()
+
+		fmt.Println("Please Enter the task category")
+		scanner.Scan()
+		category = scanner.Text()
+	}
+
 }
